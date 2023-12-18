21d44cb1c70aff54d417974bcf8cc9510fe25c5a
burak <sancakburak20@gmail.com>
2023-12-19 01:03:26 +0300

    Yeni Proje

---
+++ src/ekler.java
@@ -0,0 +1,22 @@
+public class ekler {
+
+/*
+
+
+    static String c_RESET = "\u001B[0m";
+    static String BOLD = "\033[0;1m";
+    static String c_BLUE = "\u001B[34m";
+    static String c_RED = "\u001B[31m";
+    static String c_GREEN = "\u001B[32m";
+    static String c_CYAN = "\u001B[36m";
+    static String c_PURPLE = "\u001B[35m";
+    static String c_YELLOW = "\u001B[33m";
+    static String c_WHITE = "\u001B[37m";
+    static String c_BOLD_YELLOW = "\033[1;33m";
+    static String c_UNDERLINE_CYAN = "\033[4;36m";
+
+    public static void main(String[] args) {
+
+       */
+}
+
